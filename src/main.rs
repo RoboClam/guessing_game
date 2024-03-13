bf78653052b9d33fb4ab0c62b6af8a44a662db95
jacobwright <jacob.thewrightone@gmail.com>
2024-03-13 16:03:34 -0500

    Read line from console

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+
 fn main() {
     println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    let _ = std::io::stdin().read_line(&mut guess);
+
+    println!("You guessed {}", guess);
+
 }
