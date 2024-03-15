d3b30bc6cb78b31debbde1e130d4e70667f74b2a
jacobwright <jacob.thewrightone@gmail.com>
2024-03-15 08:49:40 -0500

    Add comparison and loop

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    let secret_number = rand::thread_rng().gen_range(1..=10);
+    loop {
+        let secret_number = rand::thread_rng().gen_range(1..=10);
 
-    println!("Guess the number between 1 and 10!");
-    println!("Please input your guess.");
+        println!("I'm thinking of a number between 1 and 10!");
+        println!("What do you think it is?");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    std::io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read...");
+        std::io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read...");
 
-    println!("You guessed {guess}");
-    println!("Secret number {secret_number}");
+        let guess: u32 = guess.trim().parse().expect("Please use a number.\n");
+        println!("You guessed {guess}");
+        println!("Secret number {secret_number}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too low!"),
+            Ordering::Equal => {
+                println!("You got it!");
+                break;
+            },
+            Ordering::Greater => println!("Too high!"),
+        }
+        println!("Let's play again!");
+    }
 }
